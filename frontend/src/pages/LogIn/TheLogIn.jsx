8f3f63a8810e80fbbdaf0576135647062321d005
ofir01220 <ofircohen110@gmail.com>
2024-05-24 13:23:58 +0300

    upload

---
+++ frontend/src/pages/LogIn/TheLogIn.jsx
@@ -0,0 +1,82 @@
+const TheLogin = () =>{
+    return (
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+                <h1 className="text-3xl font-semibold text-center text-gray-300">
+                    Login
+                    <span className="text-blue-500"> ChatApp</span>
+                </h1>
+
+                <form >
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text text-gray-300">Username</span>
+                        </label>
+                        <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10 bg-black text-gray-400"/>
+                    </div>
+
+                    <div>
+                        <label className="label">
+                            <span className="text-base label-text text-gray-300">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10 bg-black text-gray-400" />
+                    </div>
+                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-gray-300">
+                        {"Don't"} have an account?
+                    </a>
+
+                    <div>
+                        <button className="btn btn-block btn-sm mt-2 bg-black text-gray-400">Login</button>
+                    </div>
+
+                </form>
+               
+            </div>
+
+        </div>
+    )
+}
+
+export default TheLogin;
+
+// STARTER CODE FOR THIS FILE.
+//const TheLogin = () =>{
+//    return (
+//        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+//            <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+//                <h1 className="text-3xl font-semibold text-center text-gray-300">
+//                    Login
+//                    <span className="text-blue-500"> ChatApp</span>
+//                </h1>
+//
+//                <form >
+//                    <div>
+//                        <label className="label p-2">
+//                            <span className="text-base label-text">Username</span>
+//                        </label>
+//                        <input type="text" placeholder="Enter username" className="w-full input input-bordered h-10 bg-black text-gray-400"/>
+//                    </div>
+//
+//                    <div>
+//                        <label className="label">
+//                            <span className="text-base label-text">Password</span>
+//                        </label>
+//                        <input type="password" placeholder="Enter Password" className="w-full input input-bordered h-10 bg-black text-gray-400" />
+//                    </div>
+//                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
+//                        {"Don't"} have an account?
+//                    </a>
+//
+//                    <div>
+//                        <button className="btn btn-block btn-sm mt-2 bg-black text-gray-400">Login</button>
+//                    </div>
+//
+//                </form>
+//               
+//            </div>
+//
+//        </div>
+//    )
+//}
+//
+//export default TheLogin;
---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,20 @@
+const GenderCheckbox = () =>{
+    return (
+        <div className="flex">
+            <div className="form-control">
+                <label className={'label gap-2 cursor-pointer'}>
+                    <span className="label-text text-gray-300">Male</span>
+                    <input type="checkbox" className="checkbox border-slate-900" />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className={'label gap-2 cursor-pointer'}>
+                    <span className="label-text text-gray-300">Female</span>
+                    <input type="checkbox" className="checkbox border-slate-900" />
+                </label>
+            </div>
+
+        </div>
+    )
+}
+export default GenderCheckbox;
